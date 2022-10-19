41ad352d4e8ae16b341230f69e45ac183f0e16c7
nonbinary00 <agarkovaalysh12@gmail.com>
2022-10-19 21:46:39 +0200

    task3 finished
    Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message, в которых сохраняй текущие значения полей формы. Пусть ключом для хранилища будет строка "feedback-form-state".
    При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. В противном случае поля должны быть пустыми.
    При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, message и текущими их значениями в консоль.
    Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.
    
    Track the input event on the form, and each time write an object with the email and message fields to the local storage, in which you save the current values ​​​​of the form fields. Let the string "feedback-form-state" be the key for the store.
    When loading the page, check the state of the storage, and if there is saved data, fill in the form fields with them. Otherwise, the fields must be empty.
    When submitting the form, clear the storage and form fields, and output an object with the fields email, message and their current values ​​to the console.
    Make sure that the storage is updated no more than once every 500 milliseconds. To do this, add to the project and use the lodash.throttle library.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,3 +6,46 @@ const { form, email, message } = {
     message: document.querySelector('textarea'),
 };
 
+const FEEDBACK_KEY = 'feedback-form-state';
+
+const storageObject = {};
+
+//Функция для сохранения входных данных в локальном хранилище
+const onInputStoreData = function (e) {
+  storageObject[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(storageObject));
+};
+
+//Прослушиватель событий с throttle для ввода формы
+form.addEventListener('input', throttle(onInputStoreData, 500));
+
+//Проверка локального хранилища и добавление данных во входные данные при перезагрузке
+const getStorageObject = localStorage.getItem('feedback-form-state');
+const parsed = JSON.parse(getStorageObject);
+
+if (getStorageObject !== null) {
+  if (parsed.email) {
+    email.value = parsed.email;
+  }
+  if (parsed.message) {
+    message.value = parsed.message;
+  }
+}
+
+//Функция отправки для сброса формы, удаления данных локального хранилища и входа в текущие данные консоли
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const submitParsed = JSON.parse(localStorage.getItem('feedback-form-state'));
+    console.log('This is current data from Local Storage', submitParsed);
+  
+    localStorage.removeItem('feedback-form-state');
+  
+    const currentData = {
+      email: email.value,
+      message: message.value,
+    };
+  
+    console.log('This is current data from form', currentData);
+  
+    form.reset();
+  });
