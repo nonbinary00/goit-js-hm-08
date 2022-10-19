6b34cdaa8883190daf613988e84d5ff32e6355ca
nonbinary00 <agarkovaalysh12@gmail.com>
2022-10-19 17:38:29 +0200

    SimpleLightbox library
    Добавь библиотеку SimpleLightbox как зависимость проекта используя npm (ссылка на CDN из твоей прошлой работы больше не нужна).
    Используй свой JavaScript код из предыдущей домашней работы, но выполни рефакторинг с учетом того, что библиотека была установлена через npm (синтаксис import/export).
    Для того чтобы подключить CSS код библиотеки в проект, необходимо добавить еще один импорт, кроме того который описан в документации.
    
    Add the SimpleLightbox library as a project dependency using npm (the CDN link from your previous work is no longer needed).
    Use your JavaScript code from previous homework, but refactor to make sure the library was installed via npm (import/export syntax).
    In order to connect the CSS code of the library to the project, you need to add one more import, in addition to the one described in the documentation.
    
    // Описан в документации
    import SimpleLightbox from "simplelightbox";
    // Дополнительный импорт стилей
    import "simplelightbox/dist/simple-lightbox.min.css";

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,32 @@ import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log(SimpleLightbox);
+// console.log(SimpleLightbox);
 
 console.log(galleryItems);
+
+const imageContainer = document.querySelector('.gallery');
+const galleryMarkup = imageMarkup(galleryItems);
+
+imageContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function imageMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description}) => {
+          return `
+          <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+              <img
+              class="gallery__image"
+              src= ${preview}
+              data-source= ${original}
+              alt= ${description}/>
+              
+          </a>
+        </div>`
+      }).join("");
+    }
+
+    const lightbox = new SimpleLightbox('.gallery a',
+    {captionsData : 'alt',
+     captionDelay : 250,
+    });
