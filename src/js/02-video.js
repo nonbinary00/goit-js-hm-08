4ada0bd06d65455cf26798843e65e51c0d6c80d7
nonbinary00 <agarkovaalysh12@gmail.com>
2022-10-19 21:21:55 +0200

    fix bug
    надо указать что бы в хранилище было уже 0 тогда оно будет сохраняться
    
    you need to specify that the storage was already 0 then it will be saved

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -16,26 +16,11 @@ const player = new Player(iframe);
   
   player.on('timeupdate', throttle(storeCurrentTime, 1000));
   
-  player
-    .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-    .then(function (seconds) {
-      // seconds = the actual time that the player seeked to
-    })
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          // the time was less than 0 or greater than the video’s duration
-          break;
-  
-        default:
-          // some other error occurred
-          break;
-      }
-    });
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
 
 
 
-    
+
 
 
 
